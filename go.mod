3eae5e461b3f967de0c457f49915997f79e32840
qaseleniumtesting01 <selenium@whitesourcesoftware.com>
2021-11-08 12:00:49 +0200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hasanwhitesource/dummy-repo-1
+
+go 1.17
---
+++ tim1/helloWorld.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	 "github.com/hasanwhitesource/dummy-repo-1/smith"
+)
+
+func main(){
+		fmt.Println("hello world")
+	        smith.SmithPrinter("yo")
+}
